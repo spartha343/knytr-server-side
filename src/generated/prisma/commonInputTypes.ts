389eac116a7c8eb8a72304a0633bc850bd705432
spartha343 <spartha343@gmail.com>
2026-02-27 22:56:53 +0600

    railway config file added

--- src/generated/prisma/commonInputTypes.ts
+++ src/generated/prisma/commonInputTypes.ts
@@ -9,8 +9,8 @@
  */
 
 import type * as runtime from "@prisma/client/runtime/client";
-import * as $Enums from "./enums.ts";
-import type * as Prisma from "./internal/prismaNamespace.ts";
+import * as $Enums from "./enums.js";
+import type * as Prisma from "./internal/prismaNamespace.js";
 
 export type StringFilter<$PrismaModel = never> = {
   equals?: string | Prisma.StringFieldRefInput<$PrismaModel>;
